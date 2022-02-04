810d9a27046b3f161db61a86c7ae0549a3a6ad26
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2022-02-04 16:02:02 +0200

    feat($lib): cleanup ast update logic

--- src/ast-replacing-logic.js
+++ src/ast-replacing-logic.js
@@ -1,8 +1,178 @@
-export const replacingLogic = (args = {}) => {
+import { prepareSpacingValueFor } from './types/spacing'
+import { prepareBoxShadowValueFor } from './types/boxShadow'
+
+const isFunction = (functionToCheck) =>
+  functionToCheck && {}.toString.call(functionToCheck) === '[object Function]'
+
+const replaceVariableInAst = (args = {}) => {
   args = {
-    variableDescriptor,
-    value,
-    ast,
+    variableDescriptor: {},
+    value: '',
+    ast: {},
     ...args,
   }
+
+  let { variableDescriptor, value, ast } = args
+
+  const newSelector = variableDescriptor.selector || ':root'
+
+  let variablePrefix = '--'
+
+  if (variableDescriptor.variableType === 'property') {
+    variablePrefix = ''
+  }
+
+  let variableName = `${variablePrefix}${
+    isFunction(variableDescriptor.variable)
+      ? variableDescriptor.variable()
+      : variableDescriptor.variable
+  }`
+
+  let hasSuchSelector = ast.rules.find(
+    ({ selector }) => selector === newSelector
+  )
+
+  const ruleToCopy = ast.rules.find(
+    ({ type, rulelist }) => type === 'ruleset' && rulelist.rules.length > 0
+  )
+
+  let newAst = JSON.parse(JSON.stringify(ast))
+
+  if (hasSuchSelector) {
+    newAst.rules = newAst.rules.map((rule) => {
+      let { selector } = rule
+
+      if (selector !== newSelector) {
+        return rule
+      }
+
+      if (
+        value.indexOf('CT_CSS_SKIP_RULE' || value.indexOf(variableName) > -1) >
+        -1
+      ) {
+        return {
+          ...rule,
+          rulelist: {
+            ...rule.rulelist,
+            rules: rule.rulelist.rules.filter(
+              ({ name }) => name !== variableName
+            ),
+          },
+        }
+      }
+
+      let hasSuchRule = rule.rulelist.rules.find(
+        ({ name }) => name === variableName
+      )
+
+      if (hasSuchRule) {
+        return {
+          ...rule,
+          rulelist: {
+            ...rule.rulelist,
+            rules: rule.rulelist.rules.map((rule) => {
+              if (rule.name === variableName) {
+                return {
+                  ...rule,
+                  value: {
+                    ...rule.value,
+                    text: value,
+                  },
+                }
+              }
+
+              return rule
+            }),
+          },
+        }
+      }
+
+      return {
+        ...rule,
+        rulelist: {
+          ...rule.rulelist,
+          rules: [
+            ...rule.rulelist.rules,
+            {
+              ...ruleToCopy.rulelist.rules[0],
+              name: variableName,
+              value: {
+                ...ruleToCopy.rulelist.rules[0].value,
+                text: value,
+              },
+            },
+          ],
+        },
+      }
+    })
+  }
+
+  if (!hasSuchSelector) {
+    newAst.rules = [
+      ...newAst.rules,
+      {
+        ...ruleToCopy,
+        selector: newSelector,
+        rulelist: {
+          ...ruleToCopy.rulelist,
+          rules: [
+            {
+              ...ruleToCopy.rulelist.rules[0],
+
+              name: variableName,
+              value: {
+                ...ruleToCopy.rulelist.rules[0].value,
+                text: value,
+              },
+            },
+          ],
+        },
+      },
+    ]
+  }
+
+  return newAst
+}
+
+export const replacingLogic = (args = {}) => {
+  args = {
+    variableDescriptor: {},
+    value: {},
+    ast: {},
+    ...args,
+  }
+
+  let actualValue =
+    (args.variableDescriptor.type || '').indexOf('color') > -1
+      ? args.value[
+          args.variableDescriptor.type === 'color'
+            ? 'default'
+            : args.variableDescriptor.type.split(':')[1]
+        ].color
+      : args.value
+
+  if ((args.variableDescriptor.type || '') === 'border') {
+    actualValue =
+      !args.value || args.value.style === 'none'
+        ? 'none'
+        : `${args.value.width}px ${args.value.style} ${args.value.color.color}`
+
+    if (args.variableDescriptor.skip_none && actualValue === 'none') {
+      actualValue = 'CT_CSS_SKIP_RULE'
+    }
+  }
+
+  if ((args.variableDescriptor.type || '') === 'spacing') {
+    actualValue = prepareSpacingValueFor(args.value)
+  }
+
+  if ((args.variableDescriptor.type || '') === 'box-shadow') {
+    actualValue = prepareBoxShadowValueFor(args.value, args.variableDescriptor)
+  }
+
+  return replaceVariableInAst({
+    variableDescriptor: args.variableDescriptor,
+    value: actualValue,
+    ast: args.ast,
+  })
 }
--- src/index.js
+++ src/index.js
@@ -1,213 +1,6 @@
-import { prepareSpacingValueFor } from './types/spacing'
-import { prepareBoxShadowValueFor } from './types/boxShadow'
-
 import { maybePromoteScalarValueIntoResponsive } from './promote-into-responsive'
-import * as shadyCss from 'shady-css-parser'
-
 import { getStyleTagsWithAst, persistNewAsts } from './ast'
-
-const isFunction = (functionToCheck) =>
-  functionToCheck && {}.toString.call(functionToCheck) === '[object Function]'
-
-const replaceVariableInAst = (args = {}) => {
-  args = {
-    variableDescriptor: {},
-    value: '',
-    device: 'desktop',
-    ast: {},
-    ...args,
-  }
-
-  let { variableDescriptor, value, device, ast } = args
-
-  const newSelector = variableDescriptor.selector || ':root'
-
-  let variablePrefix = '--'
-
-  if (variableDescriptor.variableType === 'property') {
-    variablePrefix = ''
-  }
-
-  let variableName = `${variablePrefix}${
-    isFunction(variableDescriptor.variable)
-      ? variableDescriptor.variable()
-      : variableDescriptor.variable
-  }`
-
-  let hasSuchSelector = ast.rules.find(
-    ({ selector }) => selector === newSelector
-  )
-
-  const ruleToCopy = ast.rules.find(
-    ({ type, rulelist }) => type === 'ruleset' && rulelist.rules.length > 0
-  )
-
-  let newAst = JSON.parse(JSON.stringify(ast))
-
-  console.log('here', { newAst })
-
-  if (hasSuchSelector) {
-    newAst.rules = newAst.rules.map((rule) => {
-      let { selector } = rule
-
-      if (selector !== newSelector) {
-        return rule
-      }
-
-      if (
-        value.indexOf('CT_CSS_SKIP_RULE' || value.indexOf(variableName) > -1) >
-        -1
-      ) {
-        return {
-          ...rule,
-          rulelist: {
-            ...rule.rulelist,
-            rules: rule.rulelist.rules.filter(
-              ({ name }) => name !== variableName
-            ),
-          },
-        }
-      }
-
-      let hasSuchRule = rule.rulelist.rules.find(
-        ({ name }) => name === variableName
-      )
-
-      if (hasSuchRule) {
-        return {
-          ...rule,
-          rulelist: {
-            ...rule.rulelist,
-            rules: rule.rulelist.rules.map((rule) => {
-              if (rule.name === variableName) {
-                return {
-                  ...rule,
-                  value: {
-                    ...rule.value,
-                    text: value,
-                  },
-                }
-              }
-
-              return rule
-            }),
-          },
-        }
-      }
-
-      return {
-        ...rule,
-        rulelist: {
-          ...rule.rulelist,
-          rules: [
-            ...rule.rulelist.rules,
-            {
-              ...ruleToCopy.rulelist.rules[0],
-              name: variableName,
-              value: {
-                ...ruleToCopy.rulelist.rules[0].value,
-                text: value,
-              },
-            },
-          ],
-        },
-      }
-    })
-  }
-
-  if (!hasSuchSelector) {
-    newAst.rules = [
-      ...newAst.rules,
-      {
-        ...ruleToCopy,
-        selector: newSelector,
-        rulelist: {
-          ...ruleToCopy.rulelist,
-          rules: [
-            {
-              ...ruleToCopy.rulelist.rules[0],
-
-              name: variableName,
-              value: {
-                ...ruleToCopy.rulelist.rules[0].value,
-                text: value,
-              },
-            },
-          ],
-        },
-      },
-    ]
-  }
-
-  return newAst
-}
-
-const replaceVariableInStyleTag = (args = {}) => {
-  args = {
-    variableDescriptor: {},
-    value: '',
-    device: 'desktop',
-    ...args,
-  }
-
-  persistNewAsts(
-    getStyleTagsWithAst().map((styleDescriptor) => {
-      return {
-        ...styleDescriptor,
-        ast: replaceVariableInAst({
-          ...args,
-          ast: styleDescriptor.ast,
-        }),
-      }
-    })
-  )
-}
-
-const replacingLogic = (args = {}) => {
-  const {
-    variableDescriptor,
-    value,
-    device = 'desktop',
-    ast,
-    ...remainingArgs
-  } = args
-
-  let actualValue =
-    (variableDescriptor.type || '').indexOf('color') > -1
-      ? value[
-          variableDescriptor.type === 'color'
-            ? 'default'
-            : variableDescriptor.type.split(':')[1]
-        ].color
-      : value
-
-  if ((variableDescriptor.type || '') === 'border') {
-    actualValue =
-      !value || value.style === 'none'
-        ? 'none'
-        : `${value.width}px ${value.style} ${value.color.color}`
-
-    if (variableDescriptor.skip_none && actualValue === 'none') {
-      actualValue = 'CT_CSS_SKIP_RULE'
-    }
-  }
-
-  if ((variableDescriptor.type || '') === 'spacing') {
-    actualValue = prepareSpacingValueFor(value)
-  }
-
-  if ((variableDescriptor.type || '') === 'box-shadow') {
-    actualValue = prepareBoxShadowValueFor(value, variableDescriptor)
-  }
-
-  replaceVariableInStyleTag({
-    variableDescriptor,
-    value: `${actualValue}${variableDescriptor.unit || ''}${
-      variableDescriptor.important ? ' !important' : ''
-    }`,
-    device,
-  })
-}
+import { replacingLogic } from './ast-replacing-logic'
 
 export const updateVariableInStyleTags = (args = {}) => {
   args = {
@@ -248,7 +41,6 @@ export const updateVariableInStyleTags = (args = {}) => {
             return replacingLogic({
               variableDescriptor,
               value,
-              device: 'desktop',
               ast: currentAst,
             })
           }
@@ -256,35 +48,12 @@ export const updateVariableInStyleTags = (args = {}) => {
           return replacingLogic({
             variableDescriptor,
             value: value.desktop,
-            device: 'desktop',
             ast: currentAst,
           })
         }, styleDescriptor.ast),
       }
     })
   )
-
-  console.log('here', variableDescriptor)
-
-  return
-
-  replacingLogic({
-    variableDescriptor,
-    value: value.desktop,
-    device: 'desktop',
-  })
-
-  replacingLogic({
-    variableDescriptor,
-    value: value.tablet,
-    device: 'tablet',
-  })
-
-  replacingLogic({
-    variableDescriptor,
-    value: value.mobile,
-    device: 'mobile',
-  })
 }
 
 export { clearAstCache } from './ast'
