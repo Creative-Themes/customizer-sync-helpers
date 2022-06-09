1e2e587049df9c98ed63e256e5f2be105a164175
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2022-06-10 01:58:51 +0300

    feat($general): bump versions & no longer use util

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
+import { babel } from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 import pkg from './package.json'
 
@@ -14,27 +12,25 @@ export default [
       {
         file: pkg.main,
         format: 'cjs',
-        sourcemap: true
+        sourcemap: true,
       },
 
       {
         file: pkg.module,
         format: 'es',
-        sourcemap: true
-      }
+        sourcemap: true,
+      },
     ],
     plugins: [
       external(),
-      postcss({
-        modules: true
-      }),
-      url(),
       babel({
-        exclude: 'node_modules/**'
+        exclude: 'node_modules/**',
       }),
-      resolve(),
-      commonjs()
-    ]
+      nodeResolve({
+        preferBuiltins: false,
+      }),
+      commonjs(),
+    ],
   },
 
   {
@@ -43,27 +39,25 @@ export default [
       {
         file: 'dist/simplified.js',
         format: 'cjs',
-        sourcemap: true
+        sourcemap: true,
       },
 
       {
         file: 'dist/simplified.es.js',
         format: 'es',
-        sourcemap: true
-      }
+        sourcemap: true,
+      },
     ],
     plugins: [
       external(),
-      postcss({
-        modules: true
-      }),
-      url(),
       babel({
-        exclude: 'node_modules/**'
+        exclude: 'node_modules/**',
       }),
-      resolve(),
-      commonjs()
-    ]
+      nodeResolve({
+        preferBuiltins: false,
+      }),
+      commonjs(),
+    ],
   },
 
   {
@@ -72,26 +66,22 @@ export default [
       {
         file: 'dist/promote-into-responsive.js',
         format: 'cjs',
-        sourcemap: true
+        sourcemap: true,
       },
 
       {
         file: 'dist/promote-into-responsive.es.js',
         format: 'es',
-        sourcemap: true
-      }
+        sourcemap: true,
+      },
     ],
     plugins: [
       external(),
-      postcss({
-        modules: true
-      }),
-      url(),
       babel({
-        exclude: 'node_modules/**'
+        exclude: 'node_modules/**',
       }),
-      resolve(),
-      commonjs()
-    ]
-  }
+      nodeResolve({ preferBuiltins: false }),
+      commonjs(),
+    ],
+  },
 ]
