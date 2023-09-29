f5dfbf7415d43c5d3cede176756a02013fcb067f
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2023-09-29 13:14:50 +0300

    feat($spacing): smarter backporting for spacing values

--- src/types/spacing.js
+++ src/types/spacing.js
@@ -1,10 +1,50 @@
+const SPACING_STATE_LINKED = 1
+const SPACING_STATE_INDEPENDENT = 2
+const SPACING_STATE_CUSTOM = 3
+
+const backportLegacySpacing = (legacy) => {
+  if (legacy === 'auto' || legacy === '') {
+    return {
+      value: legacy,
+      unit: '',
+    }
+  }
+
+  const maybeNumber = parseFloat(legacy)
+
+  if (isNaN(maybeNumber)) {
+    return {
+      value: '',
+      unit: '',
+    }
+  }
+
+  return {
+    value: maybeNumber,
+    unit: legacy.toString().replace(maybeNumber.toString(), ''),
+  }
+}
+
 export const prepareSpacingValueFor = (value, variableDescriptor) => {
   if (value === 'CT_CSS_SKIP_RULE') {
     return 'CT_CSS_SKIP_RULE'
   }
 
+  if (!value.values) {
+    value = {
+      values: [
+        backportLegacySpacing(value.top),
+        backportLegacySpacing(value.right),
+        backportLegacySpacing(value.bottom),
+        backportLegacySpacing(value.left),
+      ],
+      custom: '',
+      state: value.linked ? SPACING_STATE_LINKED : SPACING_STATE_INDEPENDENT,
+    }
+  }
+
   // Custom
-  if (value.state === 3) {
+  if (value.state === SPACING_STATE_CUSTOM) {
     if (!value.custom.trim()) {
       return 'CT_CSS_SKIP_RULE'
     }
