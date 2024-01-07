d0aa42fd29082e03ea27417c4515d14bc094b470
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2024-01-07 10:50:08 +0200

    chore($test): one more test

--- src/ast.test.js
+++ src/ast.test.js
@@ -59,3 +59,63 @@ test('it adds previously non-existing in the style tag', () => {
 
   expect(emptyStyle.innerText).toBe(':root{--theme-text-color:red;}')
 })
+
+test('it adds previously non-existing in the style tag with multiple variable descriptors', () => {
+  const emptyStyle = document.createElement('style')
+
+  emptyStyle.innerText = ''
+
+  emptyStyle.innerText = ':root{--theme-text-color:blue;}'
+
+  const cacheId = nanoid()
+
+  const commonArgs = {
+    cacheId,
+    initialStyleTags: [emptyStyle],
+  }
+
+  const astDescriptor = getStyleTagsWithAst(commonArgs)
+
+  const value = {
+    default: {
+      color: 'red',
+    },
+  }
+
+  // TODO: part to be refactored
+  const newAst = getUpdateAstsForStyleDescriptor({
+    variableDescriptor: [
+      {
+        selector: ':root',
+        variable: 'theme-text-color',
+        type: 'color',
+      },
+
+      {
+        selector: ':root',
+        variable: 'theme-text-color-hover',
+        type: 'color',
+      },
+
+      {
+        selector: 'body',
+        variable: 'theme-text-color-hover',
+        type: 'color',
+      },
+    ],
+
+    value,
+    fullValue: { fontColor: value },
+
+    tabletMQ: '(max-width: 800px)',
+    mobileMQ: '(max-width: 370px)',
+
+    ...commonArgs,
+  })
+
+  persistNewAsts(cacheId, newAst)
+
+  expect(emptyStyle.innerText).toBe(
+    ':root{--theme-text-color:red;--theme-text-color-hover:red;}body{--theme-text-color-hover:red;}'
+  )
+})
